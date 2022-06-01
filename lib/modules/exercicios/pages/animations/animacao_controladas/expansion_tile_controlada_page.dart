96b7629822c16124dfc3a768e0f6277aba5716c1
EduardoM <eduardommlima@outlook.com>
2022-05-31 21:43:34 -0300

    feat: foi iniciado os exercício animações

---
+++ lib/modules/exercicios/pages/animations/animacao_controladas/expansion_tile_controlada_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ExpansionTileControladaPage extends StatelessWidget {
+
+  const ExpansionTileControladaPage({ Key? key }) : super(key: key);
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text(''),),
+           body: Container(),
+       );
+  }
+}
